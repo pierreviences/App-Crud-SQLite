69a646b0175809c0f797e6b9526d921dc134beb7
pierreviences <imaniyazid@gmail.com>
2023-09-03 22:42:38 +0700

    Set adapter untuk RecyclerView pada MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,19 +3,29 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NotesDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(), this)
+        binding.notesRecycleView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecycleView.adapter = notesAdapter
         binding.addButton.setOnClickListener{
             val intent = Intent(this, AddNoteActivity::class.java)
             startActivity(intent)
 
         }
+
+
     }
 }
