edcfb727d64714c510255cf96f6fab18a5cdfda4
pierreviences <imaniyazid@gmail.com>
2023-09-03 23:18:53 +0700

    delete SQL

--- app/src/main/java/com/example/myapplication/NotesAdapter.kt
+++ app/src/main/java/com/example/myapplication/NotesAdapter.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class NotesAdapter(private var notes: List<Note>, context: Context) :
     RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+    private val db: NotesDatabaseHelper = NotesDatabaseHelper(context)
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentEditText)
         val udpateButton: ImageView = itemView.findViewById(R.id.updateButton)
+        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
 
     }
 
@@ -34,6 +38,12 @@ class NotesAdapter(private var notes: List<Note>, context: Context) :
                 putExtra("note_id", note.id)
             }
             holder.itemView.context.startActivity(intent)
+        }
+
+        holder.deleteButton.setOnClickListener {
+            db.deleteNote(note.id)
+            refreshData(db.getAllNotes())
+            Toast.makeText(holder.itemView.context, "Note Deleted", Toast.LENGTH_SHORT).show()
         }
     }
 
