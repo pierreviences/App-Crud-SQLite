e4fe8829a3448effc192cecdd24a103e973e7698
pierreviences <imaniyazid@gmail.com>
2023-09-03 21:43:34 +0700

    feat: Add Create SQL

--- app/src/main/java/com/example/myapplication/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/myapplication/NotesDatabaseHelper.kt
@@ -9,7 +9,7 @@ class NotesDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE
 
     companion object{
         private const val DATABASE_NAME = "notesapp.db"
-        private const val DATABASE_VERSION = "1"
+        private const val DATABASE_VERSION = 1
         private const val TABLE_NAME = "allnotes"
         private const val COLUMN_ID = "id"
         private const val COLUMN_TITLE = "title"
